cf75a69a80481f44bacf9ea5cbcffef78e4e853f
Carlos Jaime Mac <carlosjaimewd@gmail.com>
2019-04-01 15:35:23 -0600

    Finish code

--- Practica3/src/practica_3/Practica3.java
+++ Practica3/src/practica_3/Practica3.java
@@ -5,6 +5,10 @@
  */
 package practica_3;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.Scanner;
 /**
  *
  * @author carlosjaime
@@ -14,8 +18,49 @@ public class Practica3 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new FileReader("input.txt"));
+        StringBuilder sb = new StringBuilder();
+        StringBuilder vocalA = new StringBuilder();
+        StringBuilder vocalE = new StringBuilder();
+        StringBuilder vocalI = new StringBuilder();
+        StringBuilder vocalO = new StringBuilder();
+        StringBuilder vocalU = new StringBuilder();
+        
+        
+        
+        while(in.hasNext()) {
+            sb.append(in.next());
+        }
+        in.close();
+        String outString = sb.toString();
+        System.out.println(outString);
+        
+        for (char c : outString.toLowerCase().toCharArray()) {
+            switch (c) {
+                case 'a':
+                    vocalA.append(c);
+                break;
+                case 'e':
+                    vocalE.append(c);
+                break;
+                case 'i':
+                    vocalI.append(c);
+                break;
+                case 'o':
+                    vocalO.append(c);
+                break;
+                case 'u':
+                    vocalU.append(c);
+                break;
+            }
+        }
+        
+        System.out.println(vocalA);
+        System.out.println(vocalE);
+        System.out.println(vocalI);
+        System.out.println(vocalO);
+        System.out.println(vocalU);
     }
     
 }
